bd8861973e10d944f9dd893083ccfab9f6f7687a
125848 <125848@o365.tku.edu.tw>
2026-05-19 20:43:22 +0800

    Refactor code structure for improved readability and maintainability

---
+++ 03_JS/01_hello/01_js.js
@@ -0,0 +1,10 @@
+
+        let devmode = true; // 可以切換為 true 來啟用開發模式
+        if (devmode) {
+            
+            console.log("Hello, World!");
+            for (let i = 0; i < 5; i++) {
+                console.log("Number: " + i);
+            }
+            
+        }
